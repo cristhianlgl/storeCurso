84efac3f12d1e2a0977f137a7b0bcbc36a2bd002
Cristhian <cristhianlgl@hotmail.com>
2024-05-14 18:33:05 -0500

    feat: agregar componentes y la ruta de listas de productos

---
+++ src/app/domains/products/pages/list/list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { ProductComponent } from '../../componets/product/product.component';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [ProductComponent],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+}
