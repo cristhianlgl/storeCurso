0a1a7eab5ce4fddcdcd84d8bfb01a84b96a351f5
Cristhian <cristhianlgl@hotmail.com>
2024-05-28 17:17:03 -0500

    feat: agregar environments

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -3,6 +3,7 @@ import { CounterComponent } from '@/shared/componets/counter/counter.component';
 import { NgIf } from '@angular/common';
 import { WaveAudioComponent } from '@/info/components/wave-audio/wave-audio.component';
 import { LighlightDirective } from '@/shared/directives/lighlight.directive';
+import { environment } from '../../../../../environments/environment.development';
 
 @Component({
   selector: 'app-about',
@@ -15,6 +16,11 @@ export class AboutComponent {
   duration = signal(0);
   message = signal('hola');
   audioUrl = 'assets/audio.mp3'
+  api = environment.api;
+
+  constructor(){
+    this.api = environment.api;
+  }
 
   setDuration(event:Event){
     const valor =  event.target as HTMLInputElement
